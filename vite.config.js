bd43b571aca7411e873011cfa34e36c76c5cfe83
JG <rutadelcacomixtle@gmail.com>
2026-07-02 16:33:40 -0600

    chore: reemplazar logo por circulo_invertido.png

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'logo.png', 'logo.svg'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'logo.png'],
       manifest: {
         name: 'Bike Store',
         short_name: 'Bike Store',
